d930074d879fd272f51febdf4ec79981c5768692
Rasmus Kuusmaa <77f315@gmail.com>
2025-08-31 14:25:39 +0300

    settings page

--- naidisprojekt/Pages/SettingsPage.xaml.cs
+++ naidisprojekt/Pages/SettingsPage.xaml.cs
@@ -1,9 +1,13 @@
+using naidisprojekt.Models;
+
 namespace naidisprojekt.Pages;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
-	{
-		InitializeComponent();
-	}
+    public SettingsPage()
+    {
+        InitializeComponent();
+        Nametxt.Text = UserSession.CurrentUser.UserName;
+        Emailtxt.Text = UserSession.CurrentUser.Email;
+    }
 }
