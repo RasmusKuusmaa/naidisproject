1687f9cbfea10b36e012edf774dbc3b542a4ed0d
Rasmus Kuusmaa <77f315@gmail.com>
2025-08-31 14:08:38 +0300

    My listings

---
+++ naidisprojekt/Viewmodels/MyListingsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using naidisprojekt.Models;
+using naidisprojekt.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace naidisprojekt.Viewmodels
+{
+    internal partial class MyListingsViewModel : BaseViewModel
+    {
+        private ObservableCollection<Listing> _listings;
+        public ObservableCollection<Listing> Listings
+        {
+            get => _listings;
+            set
+            {
+                _listings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand RemoveCommand { get; }
+
+        private readonly ApiService apiService = new ApiService();
+
+        public MyListingsViewModel()
+        {
+            RemoveCommand = new Command<Listing>(RemoveListing);
+            _ = LoadListings();
+        }
+
+        private async Task LoadListings()
+        {
+            Listings = new ObservableCollection<Listing>(
+                await apiService.GetUserListings(UserSession.CurrentUser.UserId)
+            );
+        }
+
+        private void RemoveListing(Listing listing)
+        {
+            if (listing == null)
+                return;
+
+            if (Listings.Contains(listing))
+                Listings.Remove(listing);
+
+
+        }
+    }
+}
